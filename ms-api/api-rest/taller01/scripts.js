937bab7e8f3318fddffa3f0a89472e9d50cbbb9a
Edison <edygc1988@gmail.com>
2023-10-13 00:30:17 -0500

    first commit

---
+++ ms-api/api-rest/taller01/scripts.js
@@ -0,0 +1,34 @@
+const API = 'https://swapi.dev/api/people/'
+
+function personaje(texto) {
+    let div = document.createElement('div')
+    let h1_texto = document.createTextNode(texto.name)
+    let h1 = document.createElement('h1')
+    h1.appendChild(h1_texto)
+    div.appendChild(h1)
+    //let img = document.createElement('img')
+    //img.src = texto.image
+    //div.appendChild(img)
+
+    let contenedor = document.getElementById('contenedor')
+    contenedor.appendChild( div )
+}
+
+ async function obtener_personaje(id) {
+    try {
+        let response = await fetch(`${API}${id}`)
+        return personaje( await response.json() )
+    } catch(error) {
+        console.error(`[error]: ${error}`)
+    }
+    /*return new Promise((resolve, reject) => {
+        fetch(`${API}${id}`)
+            .then((data) => resolve(data.json()))
+            .catch((error) => reject(`[error]: ${error}`))
+    })*/
+
+}
+
+for (let i=1; i<=80; i++) {
+    obtener_personaje(i)
+}
